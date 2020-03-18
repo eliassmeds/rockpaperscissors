2029d2887abdafa5a8e43b8e7ead3d73e0e996ea
Elias Smeds <esmeds@uwo.ca>
2020-03-18 09:16:08 +0100

    uppdaterat test, uppdaterad readme

--- src/test/java/com/eliassmeds/rockpaperscissors/GameTest.java
+++ src/test/java/com/eliassmeds/rockpaperscissors/GameTest.java
@@ -8,61 +8,46 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-
 public class GameTest {
 
 
     @Test
-    public void joinAlreadyFullGame() throws GameFullException, NameAlreadyInUseException{
+    public void joinAlreadyFullGame() throws GameFullException, NameAlreadyInUseException {
         Game game = new Game(new Player("Elias"));
-
         game.joinGame("Lovisa");
-
         Assertions.assertThrows(GameFullException.class, () ->
                 game.joinGame("Fredrik"));
 
     }
+
     @Test
-    public void makeMoveInGameBeforePlayerTwoHasJoined(){
-        Game game=new Game(new Player("Elias"));
-            Assertions.assertThrows(GameNotFullException.class, () ->
-                    game.makeMove("Elias", "Rock"));
+    public void makeMoveInGameBeforePlayerTwoHasJoined() {
+        Game game = new Game(new Player("Elias"));
+        Assertions.assertThrows(GameNotFullException.class, () ->
+                game.makeMove("Elias", "Rock"));
 
     }
+
     @Test
-    public void makeMoveOnPlayerNotInGame(){
-        Game game=new Game(new Player("Elias"));
-        try {
-            game.joinGame("Lovisa");
-        }catch(GameFullException g){
-
-        }catch (NameAlreadyInUseException e){
-
-        }
-        Assertions.assertThrows(PlayerNotFoundException.class,()->
-                game.makeMove("Fredrik","Rock"));
+    public void makeMoveOnPlayerNotInGame() throws NameAlreadyInUseException, GameFullException {
+        Game game = new Game(new Player("Elias"));
+        game.joinGame("Lovisa");
+        Assertions.assertThrows(PlayerNotFoundException.class, () ->
+                game.makeMove("Fredrik", "Rock"));
     }
+
     @Test
-    public void testPerfectGameScenario()  {
-        Game game=new Game(new Player("Elias"));
-        try {
-            game.joinGame("Fredrik");
-            game.makeMove("Elias", "Rock");
-            game.makeMove("Fredrik", "Paper");
-        }catch(GameFullException e){
+    public void testPerfectGameScenario() throws GameFullException, NameAlreadyInUseException, GameNotFullException,
+            PlayerNotFoundException, MoveAlreadyMadeException {
+        Game game = new Game(new Player("Elias"));
+        game.joinGame("Fredrik");
+        game.makeMove("Elias", "Rock");
+        game.makeMove("Fredrik", "Paper");
 
-        }catch(NameAlreadyInUseException e){
-
-        }catch (GameNotFullException e){
-
-        }catch (PlayerNotFoundException e){
-
-        }catch(MoveAlreadyMadeException e){
-
-        }
-        Assertions.assertSame("Fredrik",game.getWinner());
+        Assertions.assertSame("Fredrik", game.getWinner());
 
     }
+
     @Test
     public void joinGameWithNameAlreadyTaken() {
         Game game = new Game(new Player("Elias"));
